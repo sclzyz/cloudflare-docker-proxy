9daff034df75fd34d22b0a464f28deb4cb7529f8
sclzyz <sc_yuanzhi@126.com>
2024-07-02 09:53:26 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.y-home.top": dockerHub,
+  "quay.y-home.top": "https://quay.io",
+  "gcr.y-home.top": "https://gcr.io",
+  "k8s-gcr.y-home.top": "https://k8s.gcr.io",
+  "k8s.y-home.top": "https://registry.k8s.io",
+  "ghcr.y-home.top": "https://ghcr.io",
+  "cloudsmith.y-home.top": "https://docker.cloudsmith.io",
+  "ecr.y-home.top": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.y-home.top": dockerHub,
 };
 
 function routeByHosts(host) {
